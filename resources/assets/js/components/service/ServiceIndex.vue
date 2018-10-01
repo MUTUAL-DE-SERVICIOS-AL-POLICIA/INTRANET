e34cf4019f4250dcdbe8828543a59a3b9d5c02f5
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-10-01 17:50:37 -0400

    Added foreach to groups and services. closed #4

--- resources/assets/js/components/service/ServiceIndex.vue
+++ resources/assets/js/components/service/ServiceIndex.vue
@@ -3,7 +3,34 @@
     <v-layout row wrap>
       <v-flex v-for="group in groups" :key="group.id" xs4>
         <v-card dark :color="group.color">
-          <v-card-text></v-card-text>
+					<v-card-title primary-title>
+						<span class="headline font-weight-bold">{{ group.name }}</span>
+					</v-card-title>
+					<v-container grid-list-md>
+						<v-layout row wrap>
+							<v-flex v-for="service in group.services" :key="service.id" xs4>
+								<v-hover>
+									<v-card
+										slot-scope="{ hover }"
+										:class="`elevation-${hover ? 20 : 2}`"
+										class="transparent"
+										:close-delay="0"
+									>
+										<a :href="service.href" style="text-decoration: none;">
+											<v-avatar	size="128">
+												<img :src="`data:image/${service.icon.format};base64,${service.icon.content}`" :alt="service.icon.name">
+											</v-avatar>
+											<v-card-text>
+												<span class="headline font-weight-light white--text">
+													{{ service.shortened }}
+												</span>
+											</v-card-text>
+										</a>
+									</v-card>
+								</v-hover>
+							</v-flex>
+						</v-layout>
+					</v-container>
         </v-card>
       </v-flex>
 		</v-layout>
@@ -15,7 +42,7 @@ export default {
   name: "ServiceIndex",
   data() {
     return {
-      groups: []
+			groups: []
     };
 	},
 	mounted() {
@@ -34,4 +61,3 @@ export default {
   }
 };
 </script>
-
