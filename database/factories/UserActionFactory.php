4f510f51e9e9d63f36592baee3d437db1b074f7b
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-10-01 14:49:56 -0400

    Fixed seeders dependencies

--- database/factories/UserActionFactory.php
+++ database/factories/UserActionFactory.php
@@ -7,7 +7,6 @@ $factory->define(App\UserAction::class, function (Faker $faker) {
 	$methods = ['POST', 'PUT', 'PATCH', 'DELETE'];
 	$routes = Route::getRoutes();
 	$paths = array();
-	$employees = App\Employee::get()->all();
 
 	foreach ($routes as $route) {
 		$paths[] = $route->uri();
@@ -17,6 +16,6 @@ $factory->define(App\UserAction::class, function (Faker $faker) {
 		'user_id' => $users[array_rand($users)]->id,
 		'method' => $methods[array_rand($methods)],
 		'path' => $paths[array_rand($paths)],
-		'data' => json_encode($employees[array_rand($employees)]),
+		'data' => json_encode([]),
 	];
 });
