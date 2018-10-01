788815eba8af9db80d4800ba2eb427e579366ff9
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-10-01 15:29:25 -0400

    Added fillable to icon model

--- app/Icon.php
+++ app/Icon.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Icon extends Model
 {
-    //
+  public $timestamps = true;
+  public $guarded = ['id'];
+  protected $dates = ['deleted_at'];
+  protected $fillable = ['name', 'content', 'format'];
 }
