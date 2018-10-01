857e052af9009e501797e1f79a82f26343d55257
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-10-01 16:35:26 -0400

    Get groups on service index view

--- resources/assets/js/components/service/ServiceIndex.vue
+++ resources/assets/js/components/service/ServiceIndex.vue
@@ -6,7 +6,25 @@
 
 <script>
 export default {
-  name: 'ServiceIndex'
-}
+  name: "ServiceIndex",
+  data() {
+    return {
+      groups: []
+    };
+  },
+  mounted() {
+    this.getGroups()
+  },
+  methods: {
+    async getGroups() {
+      try {
+        let res = axios.get("/api/v1/group");
+        this.groups = res.data;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+};
 </script>
 
