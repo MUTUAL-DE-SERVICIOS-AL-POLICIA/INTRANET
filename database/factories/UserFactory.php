be539289c8da9a565780d12ec7baaaab7963dd2f
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-10-01 15:40:57 -0400

    Added user migration. closed #5, closed #6, closed #9

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
   return [
     'username' => $faker->unique()->firstname,
-    'password' => Hash::make('secret'),
+    'password' => Hash::make('password'),
   ];
 });
