bb3139e82cc128860fa02d1c172093e73bf58ccd
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-10-02 11:48:45 -0400

    Removed colours from group

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -4,11 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Group::class, function (Faker $faker) {
   $faker->addProvider(new \Faker\Provider\Lorem($faker));
-  $faker->addProvider(new \Faker\Provider\Color($faker));
 
   return [
     'shortened' => $faker->word(),
-    'name' => $faker->sentence(),
-    'color' => $faker->hexcolor(),
+    'name' => $faker->sentence()
   ];
 });
--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -11,6 +11,6 @@ class ServiceSeeder extends Seeder
    */
   public function run()
   {
-    factory(App\Service::class, 40)->create();
+    factory(App\Service::class, 20)->create();
   }
 }
