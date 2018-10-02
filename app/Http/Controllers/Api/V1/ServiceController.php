61203fd58b49bb9c3321b6110c139def016229b5
will <wian8887@gmail.com>
2018-10-02 09:47:00 -0400

    Add service controller. closed #2

---
+++ app/Http/Controllers/Api/V1/ServiceController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Service;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ServiceController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $services = Service::get();
+    return $services;
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    return Service::create($request->all());
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Service  $service
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    return Service::find($id);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Service  $service
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $service = Service::findOrFail($id);
+    $service->fill($request->all());
+    $service->save();
+    return $service;
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Service  $service
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $service = Service::findOrFail($id);
+    $service->delete();
+    return $service;
+  }
+}
