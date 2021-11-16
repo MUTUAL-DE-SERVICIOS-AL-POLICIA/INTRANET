61f5c49ace2e3fa894fb19b2d3db28ea5a5704ca
Ale Guis F <aleguisf@gmail.com>
2021-11-16 14:41:04 -0400

    Change title

--- resources/assets/js/components/AppHeader.vue
+++ resources/assets/js/components/AppHeader.vue
@@ -3,7 +3,7 @@
     <v-img src="/img/logo.png" contain max-width="150"></v-img>
     
     <v-spacer></v-spacer>      
-    <v-toolbar-title>ESCRITORIO VIRTUAL</v-toolbar-title>
+    <v-toolbar-title>INTRANET</v-toolbar-title>
     <v-spacer></v-spacer>
     
     <v-img src="/img/escudo_bolivia.png" contain max-width="80"></v-img>
