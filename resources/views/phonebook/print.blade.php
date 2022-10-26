3c40a6e1f40200d0f936686d339c61bb08543329
Ale Guis F <aleguisf@gmail.com>
2022-10-26 15:13:39 -0400

    update print phonebook

--- resources/views/phonebook/print.blade.php
+++ resources/views/phonebook/print.blade.php
@@ -1,20 +1,20 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Telefonos internos</title>
     <link rel="stylesheet" href="{{ public_path("/css/report-print.css") }}" media="all" />
 </head>
-<body style="border: 0;border-radius: 0;">    
+<body style="border: 0;border-radius: 0;">
     <div class="block">
         <div class="font-semibold leading-tight text-md text-center m-b-10">NÚMEROS DE TELÉFONOS INTERNOS</div>
 
         <table class="table-info w-100 m-b-10">
             <thead class="bg-grey-darker">
-                <tr class="font-medium text-white text-xs uppercase">
+                <tr class="font-semibold text-white text-md uppercase">
                     <td class="px-15 text-center">Dirección</td>
                     <td class="px-15 text-center">Ubicación</td>
-                    <td class="px-15 text-center">No. Interno</td>
+                    <td class="px-15 text-center">Número Interno</td>
                 </tr>
             </thead>
             <tbody class="table-striped">
@@ -23,19 +23,19 @@
                 @foreach($position_group->locations as $key => $location)
                 <tr class="text-xs">
                     @if ($id!=$position_group->id)
-                    <td class="text-left uppercase px-5 py-3" rowspan="{{ count($position_group->locations) }}"> {{ $position_group->name }} </td>
+                    <td class="text-center px-5 py-3" rowspan="{{ count($position_group->locations) }}">{{ $position_group->shortened }}</td>
                     <td class="text-left uppercase px-5 py-3"> {{ $location->name }} </td>
                     <td class="text-center uppercase px-5 py-3"> {{ $location->phone_number }} </td>
                     @else
                     <td class="text-left uppercase px-5 py-3"> {{ $location->name }} </td>
                     <td class="text-center uppercase px-5 py-3"> {{ $location->phone_number }} </td>
-                    @endif 
+                    @endif
                 </tr>
                 @php($id=$position_group->id)
                 @endforeach
                 @endforeach
             </tbody>
-        </table>        
+        </table>       
     </div>    
 </body>
 </html>
